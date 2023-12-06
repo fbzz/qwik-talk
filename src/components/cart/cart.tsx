3549cd4f2a8ebf876ac34096507d11c5d31ac9da
fzuin <fabiozuin@outlook.com>
2023-12-06 13:51:48 +0000

    other approach

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -12,17 +12,21 @@ export default component$(({ state, totaValue }: CartProps) => {
   });
 
   return (
-    <div class="fixed bottom-0 h-16 flex w-full justify-center font-semibold">
-      <div class=" w-4/12 bg-primary h-full flex justify-between place-items-center rounded-t-md px-10 ">
-        You have {state.itemsOnCart.length} Products at the cart, Total: $
-        {totaValue}
-        <button
-          class="btn btn-secondary ml-3 text-white"
-          onClick$={() => startCheckout()}
-        >
-          Checkout
-        </button>
-      </div>
-    </div>
+    <>
+      {state.itemsOnCart.length > 0 ? (
+        <div class="fixed bottom-0 h-16 flex w-full justify-center font-semibold">
+          <div class=" w-4/12 bg-primary h-full flex justify-between place-items-center rounded-t-md px-10 ">
+            You have {state.itemsOnCart.length} Products at the cart, Total: $
+            {totaValue}
+            <button
+              class="btn btn-secondary ml-3 text-white"
+              onClick$={() => startCheckout()}
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 });
